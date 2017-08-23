34fcba6172826ef112c89e688cb4aa4b67d7cfc3
daniel <daniel.wolf@bigpanda.io>
2017-08-23 11:05:31 +0300

    Adding a count to each folder. The counts are currently calculated locally, they are not fetched from the server.

--- src/app/folders/folders-menu.component.ts
+++ src/app/folders/folders-menu.component.ts
@@ -1,23 +1,47 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 
 import {Folder} from './folder';
 import {AppState} from '../states/app-state';
 import {MoveToFolderAction} from '../actions/folders.actions';
+import {Observable} from 'rxjs/Observable';
+import {FoldersState} from '../states/folders.state';
+import {IncidentListState} from '../states/incidents.state';
 
 @Component({
   selector: 'folders-menu',
   templateUrl: './folders-menu.component.html',
   styleUrls: ['./folders-menu.component.css']
 })
-export class FoldersMenu {
+export class FoldersMenu implements OnInit {
   folders: Folder[];
   currentFolderId: string;
+  counts: Map<string, number>;
 
-  constructor(private store: Store<AppState>) {
-    store.select('folders').subscribe(foldersState => {
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    Observable.combineLatest(
+      this.store.select('folders'),
+      this.store.select('incidentList'),
+      (foldersState: FoldersState, incidentListState: IncidentListState) => {
+        const counts = new Map<string, number>();
+
+        foldersState.folders.forEach(folder => {
+          counts.set(folder.id, incidentListState.incidents.filter(incident => incident.folderId === folder.id).length);
+        });
+
+        return {
+          folders: foldersState.folders,
+          currentFolderId: foldersState.currentFolderId,
+          counts
+        };
+      }
+    )
+    .subscribe(foldersState => {
       this.folders = foldersState.folders;
       this.currentFolderId = foldersState.currentFolderId;
+      this.counts = foldersState.counts;
     });
   }
 
@@ -27,5 +51,9 @@ export class FoldersMenu {
 
   isActive(folderId: string): boolean {
     return this.currentFolderId === folderId;
+  }
+
+  getFolderCount(folderId: string): number {
+    return this.counts.get(folderId);
   }
 }
